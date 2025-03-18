a2ecf684dd777afa204bdfdc5eb2d53dd7abfa1e
Sara Colombel <sara.c77700@gmail.com>
2025-03-18 17:03:54 +0100

    favicon

--- view/view_header.php
+++ view/view_header.php
@@ -13,6 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="icon" type="image/png" href="./img/logo3.png">
     <title>CLAP Colombel</title>
 </head>
 
