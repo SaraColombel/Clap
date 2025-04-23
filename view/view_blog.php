0b991564019d6621c1b9dcfd20a340ce91d0c104
Sara Colombel <sara.c77700@gmail.com>
2025-04-23 11:59:27 +0200

    Blog Récents v2

--- view/view_blog.php
+++ view/view_blog.php
@@ -1,87 +1,102 @@
-<div class="container-fluid justify-content-center">
+<div class="container-fluid justify-content-center m-0 p-0">
 
     <!-- En-tête -->
-    <div class="row d-flex justify-content-center">
-        <div class="col-6 d-flex pt-3">
-            <div class="col-6 pe-5" style="padding-top: 12%;">
-                <h3>Découvrez l'envers du décor !</h3>
-                <h5 class="mt-3">Explorez les coulisses du cinéma et des séries, un regard affûté sur les oeuvres qui
-                    marquent notre époque. Analyse, critiques, et clins d'oeil à l'histoire du 7e art.</h5>
-                <button class="btn btn-warning btn mt-3">Découvrir</button>
-                <!-- <a href="#critiques" class="btn btn-warning mt-2">Lire les critiques</a> -->
-            </div>
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="col-6 d-flex pt-3">
+                <div class="col-6 pe-5" style="padding-top: 12%;">
+                    <h3>Découvrez l'envers du décor !</h3>
+                    <h5 class="mt-3">Explorez les coulisses du cinéma et des séries, un regard affûté sur les oeuvres qui
+                        marquent notre époque. Analyse, critiques, et clins d'oeil à l'histoire du 7e art.</h5>
+                    <button class="btn btn-warning btn mt-3">Découvrir</button>
+                    <!-- <a href="#critiques" class="btn btn-warning mt-2">Lire les critiques</a> -->
+                </div>
 
-            <div class="col-6">
-                <div class="row g-3 collage">
-                    <div class="col-md d-flex ">
-                        <img src="./img/imgBlog1.jpg" alt="Film culte 1"
-                            class="w-100 h-75 object-cover rounded mt-4 hover-zoom">
-                    </div>
+                <div class="col-6">
+                    <div class="row g-3 collage">
+                        <div class="col d-flex ">
+                            <img src="./img/imgBlog1.jpg" alt="Film culte 1"
+                                class="w-100 h-75 object-cover rounded mt-4 hover-zoom">
+                        </div>
 
-                    <div class="col-md d-flex">
-                        <div class="d-flex flex-column h-75 justify-content-between">
-                            <img src="./img/leParrain.jpg" alt="Image du haut"
-                                class="w-100 h-75 mb-3 rounded collage-img-small hover-zoom">
-                            <img src="./img/imgBlog3.jpg" alt="Image du bas"
-                                class="w-100 rounded collage-img-small hover-zoom">
+                        <div class="col d-flex">
+                            <div class="d-flex flex-column h-75 justify-content-between">
+                                <img src="./img/leParrain.jpg" alt="Image du haut"
+                                    class="w-100 h-75 mb-3 rounded collage-img-small hover-zoom">
+                                <img src="./img/imgBlog3.jpg" alt="Image du bas"
+                                    class="w-100 rounded collage-img-small hover-zoom">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+
+    <div class="container-fluid mb-5">
+        <div class="row d-flex justify-content-center">
+            <div class="col-6">
+                <h3 class="mb-3 border-bottom border-dark border-2">Récents</h3>
+                <div class="row">
+                    <div class="col-md-5 h-100">
+                        <div class="blog-card">
+                            <img id="grandeImgRecent" src="./img/grandeImgRecent.jpg" class="w-100 rounded">
+                            <p class="categorie">Tolkien</p>
+                            <div class="big-title-container">
+                                <h4 class="big-card-title">Comment Tolkien a créé un monde légendaire</h4>
+                            </div>
+                            <p class="blog-description">Dans cet article, nous plongeons dans l'univers fascinant de J.R.R. Tolkien et explorons les origines de la Terre du Milieu. Nous analyserons comment ses passions pour la mythologie, les langues et la littérature ont façonné un monde riche et complexe. Découvrez les inspirations et les processus créatifs derrière ses chefs-d'œuvre, Le Hobbit et Le Seigneur des Anneaux.</p>
+                            <p class="mb-0 datePublication">Il y a 2 semaines</p>
+                        </div>
+                    </div>
+                    <div class="col-md-7 h-100">
+                        <div class="blog-card-petit d-flex">
+                            <div class="col-3 me-2">
+                                <img src="./img/petiteImgBlog1.jpg" class="w-100 h-100 rounded object-fit-cover">
+                            </div>
+                            <div class="col-9">
+                                <p class="categorie">Tolkien</p>
+                                <div class="little-title-container">
+                                    <h4 class="little-card-title mb-2"> L'influence de son univers sur la culture populaire</h4>
+                                </div>
+                                <p class="mb-0 datePublication">Il y a 2 semaines</p>
+                            </div>
+                        </div>
+                        <div class="blog-card-petit">
+                            <div class="col-3 me-2">
+                                <img src="./img/petiteImgBlog2.jpg" class="w-100 h-100 rounded object-fit-cover">
+                            </div>
+                            <div class="col-9">
+                                <p class="categorie">Brad Pitt</p>
+                                <div class="little-title-container">
+                                    <h4 class="little-card-title mb-2">De l'icône hollywoodienne à l'activiste engagé</h4>
+                                </div>
+                                <p class="mb-0 datePublication">Il y a 2 semaines</p>
+                            </div>
+                        </div>
+                        <div class="blog-card-petit">
+                            <div class="col-3 me-2">
+                                <img src="./img/petiteImgBlog3.jpg" class="w-100 h-100 rounded object-fit-cover">
+                            </div>
+                            <div class="col-9">
+                                <p class="categorie">Jean Reno</p>
+                                <div class="little-title-container">
+                                    <h4 class="little-card-title mb-2">De l'acteur mystérieux à la star du cinéma français</h4>
+                                </div>
+                                <p class="mb-0 datePublication">Il y a 2 semaines</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
     <div class="row d-flex justify-content-center">
         <div class="col-md-6">
-            <h3 class="mb-3 border-bottom border-dark border-2">Récents</h3>
-            <div class="row justify-content-center">
-                <div class="col-md-5">
-                    <div class="blog-card">
-                        <img id="grandeImgRecent" src="./img/grandeImgRecent.jpg" class="w-100 rounded">
-                        <p class="categorie">Tolkien</p>
-                        <h4 class="card-title mb-2">Les back-stages du Seigneur des Anneaux</h4>
-                        <p class="blog-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-                            eos nobis exercitationem, enim quaerat unde facilis labore, impedit laudantium molestias
-                            consequatur tenetur quisquam, dolore aperiam rem esse incidunt tempora fugiat! </p>
-                        <p class="mb-0">Il y a 2 semaines</p>
-                    </div>
-                </div>
-                <div class="col-md-7">
-                    <div class="blog-card-petit h-25">
-                        <div class="col-md-3 me-2">
-                            <img src="./img/petiteImgBlog1.jpg" class="w-100 h-100 rounded object-fit-cover">
-                        </div>
-                        <div class="col-md-9">
-                            <p class="categorie">Tolkien</p>
-                            <h4 class="little-card-title mb-2">L'aventure fantastique mais dangereuse des 4 mousquetaires</h4>
-                            <p class="mb-0">Il y a 2 semaines</p>
-                        </div>
-                    </div>
-                    <div class="blog-card-petit h-25">
-                        <div class="col-md-3 me-2">
-                            <img src="./img/petiteImgBlog2.jpg" class="w-100 h-100 rounded object-fit-cover">
-                        </div>
-                        <div class="col-md-9">
-                            <p class="categorie">Brad Pitt</p>
-                            <h4 class="little-card-title mb-2">La carrière de Brad Pitt passée en revue</h4>
-                            <p class="mb-0">Il y a 2 semaines</p>
-                        </div>
-                    </div>
-                    <div class="blog-card-petit h-25">
-                        <div class="col-md-3 me-2">
-                            <img src="./img/petiteImgBlog3.jpg" class="w-100 h-100 rounded object-fit-cover">
-                        </div>
-                        <div class="col-md-9">
-                            <p class="categorie">Jean Reno</p>
-                            <h4 class="little-card-title mb-2">Addictions, violence, mafia, voici le vrai visage de Jean</h4>
-                            <p class="mb-0">Il y a 2 semaines</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <h3 class="mb-3 border-bottom border-dark border-2">Parcourir</h3>
         </div>
 
     </div>
-
-
-</div>
