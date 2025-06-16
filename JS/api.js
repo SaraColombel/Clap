43d1226224bad89de5108195981e57f6e215f94f
SaraColombel <sara.c77700@gmail.com>
2025-06-16 17:52:22 +0200

    Enregistrement en session des détails des films

--- JS/api.js
+++ JS/api.js
@@ -1,15 +1,23 @@
-// fichier : apiInfos.js
+// file : api.js
+
 
 export const apiKey = "9ee5cc91c2cb960c4d474ee80a467bc1";
 export const apiURLMovie = "https://api.themoviedb.org/3/movie/";
 
+/**
+ * Return API link that give informations on a specific movie
+ * @param {*} id
+ * @param {*} subroute
+ * @returns
+ */
 export function getMovieLink(id, subroute){
     return `${apiURLMovie}${id}${subroute}` + `?api_key=${apiKey}&language=fr-FR`;
 }
 
-// links list.js and home.js
+// links files list.js and home.js
 export const apiLinkAllMovies = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=fr-FR`;
 export const apiLinkGenres = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=fr-FR`;
 
-// links search.js
+// links file search.js
 export const apiLinkSearch = "https://api.themoviedb.org/3/search/movie";
+
